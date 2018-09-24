4cc0b3fbb823a1250bd394e62658a22ecb7e804c
Henri Manson <hfmanson@gmail.com>
2018-09-24 20:08:48 +0000

    new pi.sh

--- pi.sh
+++ pi.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 
-java -Dsun.security.smartcardio.library=/usr/lib/arm-linux-gnueabihf/libpcsclite.so.1 -cp dist/SmartcardIO.jar:lib/pi4j-1.2-SNAPSHOT/lib/pi4j-core.jar smartcardio.LoyalityCard slot1.p12 slot1 sim5 'ACS ACR122U PICC Interface 00 00'
+java -Dsun.security.smartcardio.library=/usr/lib/arm-linux-gnueabihf/libpcsclite.so.1 -cp dist/SmartcardIO.jar:lib/pi4j-1.2-SNAPSHOT/lib/pi4j-core.jar smartcardio.LoyalityCard slot1.p12 slot1 'ACS ACR122U PICC Interface 00 00'
