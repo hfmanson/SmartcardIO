4b74d50fd78ff76c74f01ecd17c2df14b5569d82
Henri Manson <manson@henris-mbp.mansoft>
2018-09-25 09:10:11 +0200

    updated unix script

--- run-example.sh
+++ run-example.sh
@@ -1,1 +1,3 @@
-java -cp dist/SmartcardIO.jar:lib/pi4j-1.2-SNAPSHOT/lib/pi4j-core.jar smartcardio.LoyalityCard slot1.p12 slot1 sim5
+#!/bin/sh
+
+java -cp dist/SmartcardIO.jar:lib/pi4j-1.2-SNAPSHOT/lib/pi4j-core.jar smartcardio.LoyalityCard slot1.p12 slot1
